1388ddc8962208cd1fbf5218e48edd9e718b7b20
krish <krishdu.p@gmail.com>
2022-09-22 23:01:28 +0530

    changes to make gh-pages work

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,34 +1,34 @@
-import classes from './MainNavigation.module.css';
-import { NavLink } from 'react-router-dom';
+import classes from "./MainNavigation.module.css";
+import { NavLink } from "react-router-dom";
 
 /**
- * Main navigation bar, to control navigation 
+ * Main navigation bar, to control navigation
  */
 
 const MainNavigation = () => {
-    const isActiveTab = ({isActive}) => {
-        return isActive ? classes.active : '';
-    }
+  const isActiveTab = ({ isActive }) => {
+    return isActive ? classes.active : "";
+  };
 
-    return (
-        <header className={classes.header}>
-            <div className={classes.logo}> Your Pen </div>
-            <nav className={classes.nav} >
-                <ul>
-                    <li>
-                        <NavLink className={isActiveTab} to='/quotes'>
-                            All Quotes
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={isActiveTab} to='/new-quote'>
-                            Add Quote
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}> Your Pen </div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink className={isActiveTab} to="/your-pen/quotes">
+              All Quotes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={isActiveTab} to="/your-pen/new-quote">
+              Add Quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default MainNavigation;
