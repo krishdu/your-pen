7d2abe2fc93655c66c3f30d554a6fe7a471248ba
krish <krishdu.p@gmail.com>
2022-09-22 22:42:56 +0530

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-
-import { Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
-import Quotes from './pages/Quotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
-import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import { Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import Quotes from "./pages/Quotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path = '/' element = { <Navigate to='/quotes' />} />
-        <Route path = '/quotes' element = { <Quotes />} />
-        <Route  path = '/quotes/:quoteId/*' element = {<QuoteDetail />} />
-        <Route path = '/new-quote' element = {<NewQuote />} />
-        <Route path = '*' element = {<NotFound />} />
+        <Route path="/your-pen/" element={<Navigate to="/quotes" />} />
+        <Route path="/-pen/quotes" element={<Quotes />} />
+        <Route path="/-pen/quotes/:quoteId/*" element={<QuoteDetail />} />
+        <Route path="/your-pen/new-quote" element={<NewQuote />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
