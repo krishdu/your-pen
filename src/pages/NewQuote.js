54215d5c2be41418f0828bef92a89a96c4016147
krish <krishdu.p@gmail.com>
2022-09-22 23:08:45 +0530

    gh-page config

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,24 +1,26 @@
-import { useNavigate } from 'react-router-dom';
-import QuoteForm from '../components/quotes/QuoteForm';
-import useLocalstorage from '../components/hooks/use-localstorage';
-import { addQuote } from '../components/lib/api';
-import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import QuoteForm from "../components/quotes/QuoteForm";
+import useLocalstorage from "../components/hooks/use-localstorage";
+import { addQuote } from "../components/lib/api";
+import { useEffect } from "react";
 
 const NewQuote = () => {
-    const navigate = useNavigate();
-    const {sendRequest, status} = useLocalstorage(addQuote);
+  const navigate = useNavigate();
+  const { sendRequest, status } = useLocalstorage(addQuote);
 
-    useEffect(() => {
-        if(status === 'completed'){  
-            navigate('/quotes');
-        }
-    }, [status, navigate]);
+  useEffect(() => {
+    if (status === "completed") {
+      navigate("/your-pen/quotes");
+    }
+  }, [status, navigate]);
 
-    const addQuoteHandler = (data) => {
-        sendRequest(data);
-    };
+  const addQuoteHandler = (data) => {
+    sendRequest(data);
+  };
 
-    return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}/>;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,63 +1,72 @@
-
 import { Fragment, useEffect } from "react";
-import { Route, Routes, useParams, Link, useLocation} from "react-router-dom";
-import Comments from '../components/comments/Comments';
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
-import LoadingSpinner from '../components/UI/LoadingSpinner';
-import NoQuotesFound from '../components/quotes/NoQuotesFound';
-import useLocalstorage from '../components/hooks/use-localstorage';
-import { getSingleQuote } from '../components/lib/api';
-
+import { Route, Routes, useParams, Link, useLocation } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import useLocalstorage from "../components/hooks/use-localstorage";
+import { getSingleQuote } from "../components/lib/api";
 
 /**
  * responsible for showing a particular Quote and it's behind thpught
  */
 
 const QuoteDetail = () => {
-    const params = useParams();
-    const location = useLocation();
+  const params = useParams();
+  const location = useLocation();
 
-    const {sendRequest, status, data: loadedQuote, error} = useLocalstorage(getSingleQuote, true);
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useLocalstorage(getSingleQuote, true);
 
-    const { quoteId } = params;
+  const { quoteId } = params;
 
-    useEffect(() => {
-        sendRequest(quoteId);
-    }, [sendRequest, quoteId]);
-    
-    if(status === 'pending'){
-      return <div className='centered'>
-          <LoadingSpinner />
-      </div>
-    }
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
-    if(error){
-      return <div className='centered focused'>
-          {error}
-      </div>
-    }
-
-    if(status === 'completed' && !loadedQuote.text ){
-      <NoQuotesFound />
-    }
-
+  if (status === "pending") {
     return (
-        <Fragment>
-            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}/>
-            <Routes>
-                <Route path='/' element={
-                    <div className='centered'>
-                        <Link className='btn--flat' to={`${location.pathname}/your-thought`}>
-                            Load Thoughts
-                        </Link>
-                    </div>
-                    }/>
-            </Routes>
-            <Routes>
-                <Route path='/your-thought' element={<Comments />}/>
-            </Routes>
-        </Fragment>
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
     );
+  }
+
+  if (error) {
+    return <div className="centered focused">{error}</div>;
+  }
+
+  if (status === "completed" && !loadedQuote.text) {
+    <NoQuotesFound />;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="centered">
+              <Link
+                className="btn--flat"
+                to={`${location.pathname}/your-thought`}
+              >
+                Load Thoughts
+              </Link>
+            </div>
+          }
+        />
+      </Routes>
+      <Routes>
+        <Route path="/your-thought" element={<Comments />} />
+      </Routes>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
